5fcf8e2ae2e2dd5e20e76a91a85f838163cddad7
Sergey Melnikov <39922197+MelnikovSergey@users.noreply.github.com>
2019-04-11 16:00:49 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Simple Chat-Template</title>
+	<link rel="stylesheet" href="css/simple-chat-template.css">
+</head>
+<body>
+	<h1>Файлы и папки:</h1>
+	<form action="">
+		<input type="text" name="username" placeholder="Введите ваше имя">
+		<textarea name="comment" cols="30" rows="5" placeholder="Ваш комментарий"></textarea>
+		<input type="submit">		
+	</form>
+</body>
+</html>
